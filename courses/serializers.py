6930b639ba53e89f2be359a85e7d58c97c6095d3
VeraSamohina <samohinavera44@gmail.com>
2023-10-28 12:20:46 +0300

    add lesson_list in CourseSerializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -6,9 +6,13 @@ from courses.models import Course, Lesson
 
 class CourseSerializer(serializers.ModelSerializer):
     count_lessons = SerializerMethodField()
+    lesson_list = SerializerMethodField()
 
     def get_count_lessons(self, course):
         return Lesson.objects.filter(course=course).count()
+
+    def get_lesson_list(self, course):
+        return [lesson.title for lesson in Lesson.objects.filter(course=course)]
 
     class Meta:
         model = Course
