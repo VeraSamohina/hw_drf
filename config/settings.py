eacf722c241890a5e0fefe4b43303fd18e2d8442
VeraSamohina <samohinavera44@gmail.com>
2023-11-13 09:29:07 +0300

    add celery settings

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'users',
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_celery_beat',
     'payments',
     'django_filters'
 ]
@@ -143,3 +144,7 @@ REST_FRAMEWORK = {
 }
 
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
+
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
